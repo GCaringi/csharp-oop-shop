37c2e33c4b73ba65daede997f7ceefa96a489a68
GCaringi <giannicaringi94@gmail.com>
2022-09-19 15:18:37 +0200

    Add basic behaviours

---
+++ Product.cs
@@ -0,0 +1,38 @@
+﻿namespace csharp_oop_shop;
+
+public class Product
+{ 
+    public int Code { get; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public float Vat { get; set; }
+
+    public decimal priceWithVat()
+    {
+        return Price + (Price * (decimal)Vat);
+    }
+
+    public string fullName()
+    {
+        return $"{Code} - {Name}";
+    }
+
+
+
+    public Product(string? name, string? description, decimal price, float vat)
+    {
+        Code = new Random().Next(1,10000000);
+        Name = name;
+        Description = description;
+        Price = price;
+        Vat = vat;
+    }
+
+    public Product()
+    {
+        Code = new Random().Next(1, 10000000);
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using csharp_oop_shop;
+
+Product voidProduct = new Product();
+
+Console.WriteLine(voidProduct.Vat);
+
+Product genericProduct = new Product("Rasoio", "Utilità", 24.50m, 0.22f);
+
+Console.WriteLine(genericProduct.Code);
+Console.WriteLine(genericProduct.Name);
+Console.WriteLine(genericProduct.fullName());
+Console.WriteLine(genericProduct.Description);
+Console.WriteLine(genericProduct.Price);
+Console.WriteLine(genericProduct.priceWithVat());
+
