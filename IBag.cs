49fef93a240f0107d1d73e18df6c727f65c12a59
GCaringi <giannicaringi94@gmail.com>
2022-09-22 17:55:24 +0200

    Add IBag interface

---
+++ IBag.cs
@@ -0,0 +1,8 @@
+﻿namespace csharp_oop_shop;
+
+public interface IBag
+{
+    public void Fill(Fruit fruit, int quantity);
+    public void AddFruit(Fruit fruit);
+    public void RemoveFruit(Fruit fruit);
+}
